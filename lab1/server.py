e7fad4f084eb46ddb46fbe44bb227065166ed3c2
cost1kkk <115461925+cost1kkk@users.noreply.github.com>
2024-01-20 09:23:29 +0200

    Update server.py

--- lab1/server.py
+++ lab1/server.py
@@ -13,12 +13,26 @@ def ball():
         'message3': 'foot'
     }
 
+@app.route('/gimme', methods=['GET', 'POST'])
+def easy():
+    if request.method == 'GET':
+        return {
+            'message': 'It was easy'
+        }
+    elif request.method == 'POST':
+        payload = request.json
+        print(payload)
+        return {
+            'message': 'No it wasnt'
+        }
 
 # Create a URL route in our application for "/bread"
 @app.route('/bread')
 def bread():
     return 'butter'
 
+# Start the server
+app.run()
 
 
 
